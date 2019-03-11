98eb6fe3cad7319593d52c0c9a828190703610ee
Corey H <c.l.halicke@gmail.com>
2019-03-11 02:40:26 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,7 +1,7 @@
 
 def prime?(int)
-  return false if int <= 0
-  return true if int == 2 || 3
+  return false if int <= 0 || int == 1
+  return true if int == 2 || int == 3
   2.upto(int-1) do |div|
     if int % div == 0 
       return false
