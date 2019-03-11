f4a7b9f3e9844409b3b68f91c0d6859c2f68061b
Corey H <c.l.halicke@gmail.com>
2019-03-11 02:31:01 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,8 +1,8 @@
 
 def prime?(int)
-  return false if int <= 0 
+  return false if int <= 0 || int == 2
   array = []
-  2.upto(int) do |div|
+  2.upto(int-1) do |div|
     array << int % div == 0 
   end
   
